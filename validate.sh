28164f74ef64b712c540ad2b25ef3848f9893f5a
Mostafa Moradian <mostafamoradian0@gmail.com>
2024-02-15 15:49:18 +0100

    Update script to work with the action and the sigma repo
    Update action to include the custom SIGMA_SCHEMA_PATH env-var
    Remove sigma-schema.json and download the latest version from the spec repo

--- validate.sh
+++ validate.sh
@@ -8,6 +8,26 @@ if ! command -v check-jsonschema &>/dev/null; then
     exit
 fi
 
-# Validate all the Sigma rules in the current directory
+# Set the default values for the environment variables
+if [ -z "${SIGMA_SCHEMA_PATH}" ]; then
+    SIGMA_SCHEMA_PATH="https://raw.githubusercontent.com/SigmaHQ/sigma-specification/main/sigma-schema.json"
+fi
+
+if [ -z "${SIGMA_RULES_PATH}" ]; then
+    # Get Sigma rules from the user-specified path
+    FILES=$(find ${GITHUB_WORKSPACE}/${SIGMA_RULES_PATH} -type f -name "*.yml")
+else
+    # Get Sigma rules from the default paths
+    FILES=$(find ./rules ./rules-compliance ./rules-dfir ./rules-emerging-threats ./rules-placeholder ./rules-threat-hunting -type f -name "*.yml")
+fi
+
+# If we are not running in a GitHub Action, set the default path to the repo root
+if [ -z "${GITHUB_ACTION_PATH}" ]; then
+    GITHUB_ACTION_PATH=${GITHUB_WORKSPACE}
+fi
+
+echo "Downloading the latest version of the sigma-schema.json"
+wget ${SIGMA_SCHEMA_PATH} -O ${GITHUB_ACTION_PATH}/sigma-schema.json
+
 echo "Validating Sigma rules against sigma-schema.json"
-check-jsonschema --schemafile ${GITHUB_ACTION_PATH}/sigma-schema.json $(find ${GITHUB_WORKSPACE}/${SIGMA_RULES_PATH} -type f -name "*.yml")
+check-jsonschema --schemafile ${GITHUB_ACTION_PATH}/sigma-schema.json ${FILES}
