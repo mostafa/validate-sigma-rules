97f143cff443d2f2362de3cb0fc89165a83dde18
Mostafa Moradian <mostafamoradian0@gmail.com>
2024-02-16 16:46:18 +0100

    Update action to add more parameters
    Update script to handle more errors and consider more corner cases
    Update README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This action is used to validate Sigma rules using the JSON schema. It is used to ensure that the rules are correctly formatted and that they will work with the Sigma converter.
 
-## Basic usage
+## Usage
 
 For an example repository structure, see the [example repository](https://github.com/mostafa/validate-sigma-rules-example).
 
@@ -10,27 +10,57 @@ For an example repository structure, see the [example repository](https://github
 steps:
     - uses: mostafa/validate-sigma-rules@v0
       with:
-        path: './'
-        schemaPath: 'https://raw.githubusercontent.com/SigmaHQ/sigma-specification/main/sigma-schema.json'
+        paths: './'
+        schemaURL: 'https://raw.githubusercontent.com/SigmaHQ/sigma-specification/main/sigma-schema.json'
+```
+
+If you want to use an existing schema, you can use the `schemaFile` input:
+
+```yaml
+steps:
+    - uses: mostafa/validate-sigma-rules@v0
+      with:
+        paths: './'
+        schemaFile: './sigma-schema.json'
+```
+
+The `paths` can be used to provide multiple paths to the rules. For example, if you have a `rules` directory and a `custom-rules` directory, you can use the following:
+
+```yaml
+steps:
+    - uses: mostafa/validate-sigma-rules@v0
+      with:
+        paths: |-
+          ./rules
+          ./custom-rules
 ```
 
 ## Inputs
 
-### `path` (optional)
+### `paths` (optional)
 
-The path to the Sigma rules in your repository. This is a optional input and should be a relative path to the root of the repository. The default value is `./`.
+The path(s) to the Sigma rules in your repository. This is a optional input and should be a relative path to the root of the repository. The default value is `./`.
 
-### `schemaPath` (optional)
+### `schemaURL` (optional)
 
-The path to the JSON schema. This is an optional input and should be a URL to the JSON schema. The default value is using the latest version of the Sigma schema from the [sigma-specification repository](https://github.com/SigmaHQ/sigma-specification).
+The URL to the latest version of the JSON schema for Sigma (or any other version or commit). This is an optional input and should be a URL to the JSON schema. The default value can be the latest version of the Sigma schema from the [sigma-specification repository](https://github.com/SigmaHQ/sigma-specification):
+
+```yaml
+...
+schemaURL: 'https://raw.githubusercontent.com/SigmaHQ/sigma-specification/main/sigma-schema.json'
+...
+```
+
+### `schemaFile` (optional)
+
+The path to the JSON schema for Sigma. This is an optional input and should be a relative path to the root of the repository.
+
+> [!IMPORTANT]
+> Either `schemaURL` or `schemaFile` should be provided. If both are provided, an error will be thrown with a non-zero exit code. If none are provided, the action will use the default schema from the [sigma-specification repository](https://github.com/SigmaHQ/sigma-specification), as mentioned above.
 
 ## Outputs
 
 It returns a zero exit code if the validation is successful and a non-zero exit code and errors if the validation fails.
-
-## Limitations
-
-It currently only supports a single path to the rules. It does not support multiple paths or globs (for now).
 
 ## License
 
--- validate.sh
+++ validate.sh
@@ -5,20 +5,29 @@ if ! command -v check-jsonschema &>/dev/null; then
     echo "check-jsonschema could not be found"
     echo "Please install it from PyPI using:"
     echo "pip install check-jsonschema"
-    exit
+    exit 1
 fi
 
-# Set the default values for the environment variables
-if [ -z "${SIGMA_SCHEMA_PATH}" ]; then
-    SIGMA_SCHEMA_PATH="https://raw.githubusercontent.com/SigmaHQ/sigma-specification/main/sigma-schema.json"
+# Set the GITHUB_WORKSPACE environment variable to the current directory if it is not set
+if [ -z "${GITHUB_WORKSPACE}" ]; then
+    GITHUB_WORKSPACE="./"
 fi
 
+# for every newline-separated path in SIGMA_RULES_PATH, remove the newline and replace it with a space. Also remove any leading or trailing whitespace and remove ./ and // from the path
+if [ -n "${SIGMA_RULES_PATH}" ]; then
+    SIGMA_RULES_PATH=$(echo ${SIGMA_RULES_PATH} | sed 's/\\n/ /g' | sed 's/\/\/$/\//g' | sed 's/\.\/\.\//\.\//g' | awk '{$1=$1};1')
+fi
+
+# Convert newline-separated paths to space-separated paths
 if [ -n "${SIGMA_RULES_PATH}" ]; then
     # Get Sigma rules from the user-specified path
-    FILES=$(find ${GITHUB_WORKSPACE}/${SIGMA_RULES_PATH} -type f -name "*.yml")
-else
-    # Get Sigma rules from the default paths
-    FILES=$(find ./rules ./rules-compliance ./rules-dfir ./rules-emerging-threats ./rules-placeholder ./rules-threat-hunting -type f -name "*.yml")
+    PATHS=$(echo ${SIGMA_RULES_PATH} | awk -v ghws="${GITHUB_WORKSPACE}" '{print ghws$0}' | sed 's/\.\/\.\//\.\//g' | awk '{$1=$1};1')
+    FILES=$(find ${PATHS} -type f -name "*.yml")
+fi
+
+if [ -z "${FILES}" ]; then
+    echo "No Sigma rules found, please set the SIGMA_RULES_PATH environment variable"
+    exit 1
 fi
 
 # If we are not running in a GitHub Action, set the default path to the repo root
@@ -26,8 +35,30 @@ if [ -z "${GITHUB_ACTION_PATH}" ]; then
     GITHUB_ACTION_PATH=${GITHUB_WORKSPACE}
 fi
 
-echo "Downloading the latest version of the sigma-schema.json"
-wget ${SIGMA_SCHEMA_PATH} -O ${GITHUB_ACTION_PATH}/sigma-schema.json
+if [ ! ${SIGMA_SCHEMA_URL} == "" ] && [ ! ${SIGMA_SCHEMA_FILE} == "" ]; then
+    echo "Both SIGMA_SCHEMA_URL and SIGMA_SCHEMA_FILE are set"
+    echo "Please only set one of these environment variables"
+    exit 1
+fi
+
+# Local schema file is preferred over the URL
+if [ -n ${SIGMA_SCHEMA_FILE} ]; then
+    echo "Using the local sigma-schema.json"
+    if [ ! -f "${GITHUB_ACTION_PATH}/sigma-schema.json" ]; then
+        echo "The local sigma-schema.json does not exist."
+        echo "Please download it from ${SIGMA_SCHEMA_URL}"
+        echo "and place it in the ${GITHUB_ACTION_PATH} directory"
+        echo "or set the USE_LOCAL_SCHEMA environment variable to false"
+        exit 1
+    fi
+else
+    # Set the SIGMA_SCHEMA_URL environment variable to the default Sigma schema path if it is not set
+    if [ -z "${SIGMA_SCHEMA_URL}" ]; then
+        SIGMA_SCHEMA_URL="https://raw.githubusercontent.com/SigmaHQ/sigma-specification/main/sigma-schema.json"
+    fi
+    echo "Downloading the latest version of the sigma-schema.json"
+    wget ${SIGMA_SCHEMA_URL} -O ${GITHUB_ACTION_PATH}/sigma-schema.json
+fi
 
 echo "Validating Sigma rules against sigma-schema.json"
 check-jsonschema --schemafile ${GITHUB_ACTION_PATH}/sigma-schema.json ${FILES}
